61479e6df1598ea359c062c3437ae5b3e9abcdb0
jffguo11 <jffguo11@gmail.com>
2016-07-26 19:42:59 +0000

    python_01

--- loop.py
+++ loop.py
@@ -1,3 +1,5 @@
+import math
+
 abc = [3, 34, 78.33, 234, 5]
 
 deg = [3251, 1345, 68, 132509]
@@ -9,10 +11,51 @@ for x in abc:
         print x
     else:
         print(str(x) + " " + "is not greater than or equal to 50")
-
+#-----------------------------------------------------------
 
 def counter(topv):
     for x in range(1,(topv + 1)):
         print x
 
 counter(5)
+#-----------------------------------------------------------
+
+def findevens(lyst):
+    for x in lyst:
+        if x % 2 == 0:
+            print(str(x) + " is even")
+
+lyst1 = [32, 57, 94, 1008, 761]
+findevens(lyst1)
+#-----------------------------------------------------------
+
+arr1 = [1, 42, 56, 71, 39, 55, 901]
+arr2 = [2,1,4,65,776,79,321]
+arr3 = arr1 + arr2
+print arr3
+
+def separr(lyst):
+    """takes in an array and loops through each element
+       if the element is even or equal to 1, say that the element is even or equal to one
+       else(if the element is odd), say the element is odd """
+    for x in lyst:
+        if x % 2 == 0 or x == 1:
+            print(str(x) + " is even or equal to one")
+        elif x % 2 == 1:
+            print(str(x) + " is odd and not equal to one")
+separr(arr3)
+
+#-----------------------------------------------------------
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(7))
+
+
+
+
+
